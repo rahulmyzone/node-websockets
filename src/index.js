04a35467b6dd372378a993a22b17527cce7c8335
Rahul Dhiman <rahulmyzone@gmail.com>
2022-08-07 09:16:43 +0530

    Added endpoints for websockets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 
 const app = express();
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello, world!')
-    .end();
-});
- 
+const routes = require("./routes");
+app.use(express.json());
+app.use(routes);
+
 // Start the server
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
